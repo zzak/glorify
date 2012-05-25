1a76fea9d3307c3ba1b65d1a9a48e9d417516940
Zachary Scott <zachary@zacharyscott.net>
2012-05-25 14:17:59 -0400

    redundant initialize methods

--- lib/glorify/template.rb
+++ lib/glorify/template.rb
@@ -4,14 +4,6 @@ require 'tilt/template'
 module Sinatra
   module Glorify
     class Template < Tilt::Template
-      def self.engine_initialized?
-        defined? Glorify::Template
-      end
-
-      def initialize_engine
-        require_template_library 'glorify/template'
-      end
-      
       def prepare
         @engine = Redcarpet::Markdown.new(Glorify::Renderer.new,
                                           Glorify::EXTENSIONS)
