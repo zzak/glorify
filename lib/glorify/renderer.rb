862d6955a5c19c86c296e8dfae4e943a28a75776
Zachary Scott <zachary@zacharyscott.net>
2012-12-04 02:32:49 -0500

    remove RubyPython dependency, #8

--- lib/glorify/renderer.rb
+++ lib/glorify/renderer.rb
@@ -5,18 +5,8 @@ module Sinatra
   module Glorify
     class Renderer < Redcarpet::Render::HTML
 
-      def initialize(options={})
-        python = ENV['GLORIFY_PYTHON'] || 'python'
-        RubyPython.configure :python_exe => python
-        super
-      end
-
       def block_code(code, lang)
-        begin
-          Pygments.highlight(code, :lexer => lang, :options => {:encoding => "utf-8"})
-        rescue RubyPython::PythonError
-          Pygments.highlight(code, :options => {:encoding => "utf-8"})
-        end
+        Pygments.highlight(code, :lexer => lang, :options => {:encoding => "utf-8"})
       end
 
     end
